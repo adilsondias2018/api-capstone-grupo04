6f2244abf95dd7f4f52f8153d930fc5c1f3f9bf2
Adilson Dias <adilsondias@outlook.com>
2021-01-21 18:47:11 -0300

    chore: Auteraçao tabela cart e acresentou-se a tabela products

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const rules = auth.rewriter({
   "/users*": "/600/users$1",
   "/produtos*": "/644/produtos$1",
   "/categorias*": "/644/categorias$1",
-  "/card*": "/644/cart$1",
+  "/cart*": "/644/cart$1",
+  "/products*": "/644/products$1",
 });
 
 app.use(cors());
