0dfab2be12183063ced6cdb4d9e0a10cdcc65d2e
Adilson Dias <adilsondias@outlook.com>
2021-01-21 14:56:30 -0300

    chore: Mudança nome da rota

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const rules = auth.rewriter({
   "/users*": "/600/users$1",
   "/produtos*": "/644/produtos$1",
   "/categorias*": "/644/categorias$1",
-  "/card*": "/644/card$1",
+  "/card*": "/644/cart$1",
 });
 
 app.use(cors());
